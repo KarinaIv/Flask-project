f796a5e538ab241a9da7d3befab254c0557017ea
annakostikova1 <96978296+annakostikova1@users.noreply.github.com>
2022-05-07 19:11:56 +0300

    Add files via upload

---
+++ our_Ak_project/data/likes.py
@@ -0,0 +1,23 @@
+
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class likes(SqlAlchemyBase):
+    __tablename__ = 'cart'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+
+    id_products = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    cnt = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    cart_or_likes = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
+    products = orm.relation('products')
+
