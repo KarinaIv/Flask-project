3ae6b49afaa2c68035ae422e7601d7b77ff1d89f
annakostikova1 <96978296+annakostikova1@users.noreply.github.com>
2022-05-10 20:37:26 +0300

    Add files via upload

---
+++ our_Ak_project/data/cart.py
@@ -0,0 +1,19 @@
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Cart(SqlAlchemyBase):
+    __tablename__ = 'cart'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+
+    id_products = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("products.id"))
+    cnt = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    # cart_or_likes = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
+    products = orm.relation('Products')
+
