b2676830b2352fd5d840e5646da6ac69f3f523fc
annakostikova1 <96978296+annakostikova1@users.noreply.github.com>
2022-05-10 21:50:15 +0300

    Add files via upload

--- our_Ak_project/forms/products.py
+++ our_Ak_project/forms/products.py
@@ -1,3 +1,4 @@
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, FileField
 from wtforms import BooleanField, SubmitField
@@ -9,5 +10,5 @@ class ProductsForm(FlaskForm):
     title = StringField('Название товара', validators=[DataRequired()])
     content = TextAreaField("Описание товара")
     price = FloatField('Цена товара', validators=[DataRequired()])
-    image = FileField('Изображение товара', validators=[DataRequired()])
+    image = FileField('Изображение товара')
     submit = SubmitField('Применить')
